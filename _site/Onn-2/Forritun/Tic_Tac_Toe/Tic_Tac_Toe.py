c67dac6444988be5c612f37cac22e5eafc19e26e
Guðmundur Björnsson <brimir27@outlook.com>
2019-03-16 10:23:45 +0000

    Auglýsingu er bætt við

--- _site/Onn-2/Forritun/Tic_Tac_Toe/Tic_Tac_Toe.py
+++ _site/Onn-2/Forritun/Tic_Tac_Toe/Tic_Tac_Toe.py
@@ -1,13 +1,15 @@
 import turtle
-import time
+import itertools
 
+global flag
+global takki
 flag = False
-flag_1 = False
+takki = 0
 
-bord = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+bord = ["-", "-", "-","-", "-", "-","-", "-", "-"]
+stadsetningar = [[-290, 290], [-90, 290], [110, 290], [-290, 90], [-90, 90], [110, 90], [-290, -110], [-90, -110], [110, -110]]
 
-global spilari
-spilari = 1
+spilari = itertools.cycle(["x", "y"])
 
 # Skjár
 wn = turtle.Screen()
@@ -19,6 +21,8 @@ grid = turtle.Turtle()
 grid.speed(0)
 grid.pencolor("black")
 grid.right(90)
+
+wn.tracer(0)
 
 for x in range(2):
     grid.penup()
@@ -32,8 +36,7 @@ for x in range(2):
     grid.pendown()
     grid.forward(600)
 grid.hideturtle()
-
-wn.tracer(0)
+wn.update()
 
 #Teiknari
 teiknari = turtle.Turtle()
@@ -44,73 +47,98 @@ teiknari.pencolor("black")
 # Innsláttur
 #  1 Röð
 def grid_1():
-    if(bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(-290, 290)
-        teiknari.pendown()
+    if(bord[0] == "-"):
+        global takki
+        takki = 0
         teikna()
 
 def grid_2():
     if (bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(-90, 290)
-        teiknari.pendown()
+        global takki
+        takki = 1
         teikna()
 
 def grid_3():
-    if (bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(110, 290)
-        teiknari.pendown()
+    if (bord[2] == "-"):
+        global takki
+        takki = 2
         teikna()
 
 #2 Röð
 def grid_4():
-    if (bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(-290, 90)
-        teiknari.pendown()
+    if (bord[3] == "-"):
+        global takki
+        takki = 3
         teikna()
 
 def grid_5():
-    if (bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(-90, 90)
-        teiknari.pendown()
+    if (bord[4] == "-"):
+        global takki
+        takki = 4
         teikna()
 
 def grid_6():
-    if (bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(110, 90)
-        teiknari.pendown()
+    if (bord[5] == "-"):
+        global takki
+        takki = 5
         teikna()
 
 #3 Röð
 def grid_7():
-    if (bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(-290, -110)
-        teiknari.pendown()
+    if (bord[6] == "-"):
+        global takki
+        takki = 6
         teikna()
 
 def grid_8():
-    if (bord[1] == "-"):
-        teiknari.penup()
-        teiknari.goto(-90, -110)
-        teiknari.pendown()
+    if (bord[7] == "-"):
+        global takki
+        takki = 7
         teikna()
 
 def grid_9():
-    teiknari.penup()
-    teiknari.goto(110, -110)
-    teiknari.pendown()
-    teikna()
+    if(bord[8] == "-"):
+        global takki
+        takki = 8
+        teikna()
+
+
+# Gáir hvort að einhver sé búinn að vinna.
+def VinnaTjekkari(takn):
+    global  flag
+    for x in range(3):
+        if(bord[0+x*3] == takn and bord[1+x*3] == takn and bord[2+x*3] == takn):
+            flag = True
+            print("Ragnar")
+        elif(bord[0+x] == takn and bord[3+x] == takn and bord[6+x] == takn):
+            flag = True
+            print("Ragnar")
+    if(bord[0] == takn and bord[4] == takn and bord[8] == takn):
+        flag = True
+        print("Ragnar")
+    elif(bord[2] == takn and bord[4] == takn and bord[6] == takn):
+        flag = True
+        print("Ragnar")
+    teljari = 0
+    for x in range(9):
+        if(bord[x] != "-"):
+            teljari += 1
+    if(teljari == 9):
+        flag = True
 
 # Teikna
 def teikna():
     global spilari
-    if(spilari % 2 == 0):
+    hnit = stadsetningar[takki]
+
+    teiknari.penup()
+    teiknari.goto(hnit[0], hnit[1])
+    teiknari.pendown()
+
+    global nuverandi
+    nuverandi = next(spilari)
+    bord[takki] = nuverandi;
+    if(nuverandi == "x"):
         # Kross
         x = teiknari.xcor()
         y = teiknari.ycor()
@@ -128,8 +156,8 @@ def teikna():
         teiknari.goto(teiknari.xcor()+90, teiknari.ycor()-180)
         teiknari.pendown()
         teiknari.circle(90)
+    VinnaTjekkari(nuverandi)
 
-    spilari += 1
 
 # Lyklaborð
 wn.listen()
@@ -145,6 +173,30 @@ wn.onkeypress(grid_8, "2")
 wn.onkeypress(grid_9, "3")
 
 while(True):
-    wn.update()
+    if(flag != True):
+        wn.update()
+    else:
+        wn.update()
+        if(wn.textinput("Leikur kláraður", "Viltu  spila annan leik? : ").lower() == "já"):
+            bord = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+            teiknari.reset()
+            flag = False
+
+            if(nuverandi == "x"):
+                next(spilari)
+            wn.listen()
+
+            wn.onkeypress(grid_1, "7")
+            wn.onkeypress(grid_2, "8")
+            wn.onkeypress(grid_3, "9")
+            wn.onkeypress(grid_4, "4")
+            wn.onkeypress(grid_5, "5")
+            wn.onkeypress(grid_6, "6")
+            wn.onkeypress(grid_7, "1")
+            wn.onkeypress(grid_8, "2")
+            wn.onkeypress(grid_9, "3")
+        else:
+            wn.done()
+            break
 
 wn.mainloop()
